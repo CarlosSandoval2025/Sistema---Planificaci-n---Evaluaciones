25b179f8122860a1dd1ccca0ea3391f648faeb61
Carlos Sandoval2025 <carlos.j98232s.s@gmail.com>
2026-02-18 21:17:43 -0500

    Se agrego modelo Evaluaciones con atributos y metodos

---
+++ Src/Modelos/EvaluacionAcademica.ts
@@ -0,0 +1,24 @@
+class EvaluacionAcademica{
+    private id: number;
+    private titulo: string;
+    private tipo: string;
+    private fecha: Date;
+    private duracionMin: number;
+    private estado: string;
+
+    constructor(id: number, titulo: string, tipo: string, fecha: Date, duracionMin: number, estado: string){
+        this.id = id;
+        this.titulo = titulo;
+        this.tipo = tipo;
+        this.fecha = fecha;
+        this.duracionMin = duracionMin;
+        this.estado = estado;
+    }
+
+    getResumen(): string{
+        return this.titulo + "\n" + "Tipo: " + this.tipo + "\n" + "Fecha: " + this.fecha.toLocaleDateString + "\n" +
+        "Duracion: " + this.duracionMin + "min\n" + "Estado: " + this.estado;
+
+    }
+
+}
