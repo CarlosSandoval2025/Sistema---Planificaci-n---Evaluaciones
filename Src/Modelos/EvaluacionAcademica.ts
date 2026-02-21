9738472b07c1a9bba70f7eb257212f147f0f8f88
Carlos Sandoval2025 <carlos.j98232s.s@gmail.com>
2026-02-20 23:39:21 -0500

    Se agrega clase Docente

--- Src/Modelos/EvaluacionAcademica.ts
+++ Src/Modelos/EvaluacionAcademica.ts
@@ -18,7 +18,6 @@ class EvaluacionAcademica{
     getResumen(): string{
         return this.titulo + "\n" + "Tipo: " + this.tipo + "\n" + "Fecha: " + this.fecha.toLocaleDateString + "\n" +
         "Duracion: " + this.duracionMin + "min\n" + "Estado: " + this.estado;
-
     }
 
 }
