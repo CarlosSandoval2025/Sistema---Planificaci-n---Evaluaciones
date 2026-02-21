545cebf1adfa7c7eb1578054c27f7aea5905050e
Carlos Sandoval2025 <carlos.j98232s.s@gmail.com>
2026-02-20 23:41:39 -0500

    Se agrego Clase Docente

---
+++ Src/Modelos/Docente.ts
@@ -0,0 +1,20 @@
+class Docente{
+    private id: number;
+    private nombre: string;
+    private correo: string;
+    private especialidad: string;
+
+    constructor(id: number, nombre: string, correo: string, especialidad: string){
+        this.id = id; 
+        this.nombre = nombre;
+        this.correo = correo;
+        this.especialidad = especialidad;
+    }
+
+    getResumen(): string{
+        return "Docente: " + this.nombre + "\n" + 
+        "Correo: " + this.correo + "\n" +
+        "Especialidad: " + this.especialidad + "\n";
+    }
+
+}
