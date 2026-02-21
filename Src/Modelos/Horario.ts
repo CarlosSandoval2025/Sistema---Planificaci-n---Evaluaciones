1c29c0e227da84dccf5cd3e0008fd8c1d3f310ca
Carlos Sandoval2025 <carlos.j98232s.s@gmail.com>
2026-02-21 00:00:33 -0500

    Se agrego la clase Horario

---
+++ Src/Modelos/Horario.ts
@@ -0,0 +1,22 @@
+class Horario{
+    private id: number;
+    private dia: string;
+    private horaInicio: string;
+    private horaFin: string;
+    private aula: string;
+
+    constructor(id: number, dia: string, horaInicio: string, horaFin: string, aula: string){
+        this.id = id;
+        this.dia =dia;
+        this.horaInicio = horaInicio;
+        this.horaFin =horaFin;
+        this.aula = aula;
+    }
+
+    getResumen(): string{
+        return "Dia: " + this.dia + "\n" + 
+        "Inicio: " + this.horaInicio + "\n" +
+        "Fin: " + this.horaFin + "\n" +
+        "Aula: " + this.aula + "\n" ;
+    }
+}
